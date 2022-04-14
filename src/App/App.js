067023d32e9b98a8734d729d37caff1a38c0b7f1
Vũ Đức Tuyên <vutuyenkt2000@gmail.com>
2022-04-14 13:20:34 +0700

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SideMenu from '../components/SideMenu';
+const App = ()=> {
+  return (
+    <>
+      <SideMenu />
+      <div>Here we gooo</div>
+    </>
+
+
+  );
+}
+
+export default App;
